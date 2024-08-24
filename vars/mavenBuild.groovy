088c84179a91b234df5b1b0fd8b56af2137b7ebc
Amir Moharam <amir.moharam@gmail.com>
2024-08-25 06:29:45 +1200

    refactor(pipeline): use explicit Maven tool configuration
    - Replace dynamic `tool` method with static `String` assignment for Maven tool.
    - Configure Maven tool using `tools` block and `withEnv` for environment setup.
    - Update `Checkout` stage configuration for improved readability.
    - Enhance error handling with clear success and failure messages.
    
    Improves pipeline clarity and ensures consistent Maven tool usage.

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -6,41 +6,40 @@ def call(Map config) {
         String branch = 'master'
 
         // Tool configuration
-        def mavenTool = tool name: 'Maven 3.9.8'
-
-        try {
-            stage('Checkout') {
-                // Checkout code from GitHub
-                checkout(
-                    [$class: 'GitSCM', branches: [[name: branch]],
+        String mavenTool = 'Maven 3.9.8'
+        withEnv(["PATH+MAVEN=${mavenTool}/bin"]) {
+            try {
+                stage('Checkout') {
+                    // Checkout code from GitHub
+                    checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: branch]],
                     userRemoteConfigs: [[url: "${gitBaseUrl}/${config.name}", credentialsId: gitCredentialsId]]])
-            }
+                }
 
-            stage('Build') {
-                withEnv(["PATH+MAVEN=${mavenTool}/bin"]) {
+                stage('Build') {
+                    tools {maven mavenTool}
                     sh 'mvn clean package'
                 }
-            }
 
-            stage('Test') {
-                withEnv(["PATH+MAVEN=${mavenTool}/bin"]) {
+                stage('Test') {
                     sh 'mvn test'
                 }
-            }
 
-            stage('Deploy') {
-                echo 'Deploying application...'
-            // Add deployment steps here
-            }
+                stage('Deploy') {
+                    echo 'Deploying application...'
+                    // Add deployment steps here
+                }
         } catch (Exception ex) {
-            currentBuild.result = 'FAILURE'
-            echo 'Pipeline failed.'
-            throw ex
+                currentBuild.result = 'FAILURE'
+                echo 'Pipeline failed.'
+                throw ex
         } finally {
-            if (currentBuild.result == 'SUCCESS') {
-                echo 'Pipeline succeeded!'
+                if (currentBuild.result == 'SUCCESS') {
+                    echo 'Pipeline succeeded!'
+                }
+                echo 'Cleaning up...'
             }
-            echo 'Cleaning up...'
         }
     }
 }
