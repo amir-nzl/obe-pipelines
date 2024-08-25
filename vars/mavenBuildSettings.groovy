82f2defa7792a49507666c13093b298a44d82adc
Amir Moharam <amir.moharam@gmail.com>
2024-08-25 20:23:09 +1200

    refactor(mavenBuildSettings): remove redundant stage declaration
    - Removed redundant stage declaration 'Fetch Maven Settings' from `mavenBuildSettings` method.
    - Updated `mavenBuildSettings` to directly execute commands without nesting them in a stage.

--- vars/mavenBuildSettings.groovy
+++ vars/mavenBuildSettings.groovy
@@ -1,11 +1,9 @@
 void call(String creds='github-token', String repository='', String branch='master') {
-    stage('Fetch Maven Settings') {
-        dir('dependency') {
+    dir('dependency') {
             git(url: repository, branch: branch, credentialsId: creds)
             sh """
             ls -l
             cp maven-settings/settings.xml ${env.WORKSPACE}/settings.xml
             """
-        }
     }
 }
