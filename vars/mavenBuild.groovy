74176a8005e96a09060c95ad813c7b8da51ae119
Amir Moharam <amir.moharam@gmail.com>
2024-08-26 07:02:25 +1200

    feat: add Nexus credentials for Maven builds
    - Updated `settings.xml` to include Nexus server credentials for both releases and snapshots using environment variables.
    - Modified `mavenBuild.groovy` to fetch Maven settings with credentials securely via Jenkins credentials (`nexus-deploy`).

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -9,37 +9,41 @@ void call(Map config) {
         String mavenTool = tool name: 'Maven 3.9.8'
 
         withEnv(["PATH+MAVEN=${mavenTool}/bin"]) {
-            try {
-                stage('Checkout') {
-                    // check out the source code from the same repository that the Jenkinsfile resides in.
-                    checkout scm
-                }
+            withCredentials([
+                usernamePassword(credentialsId: 'nexus-deploy', usernameVariable: 'MAVEN_NEXUS_USERNAME', passwordVariable: 'MAVEN_NEXUS_PASSWORD')
+            ]) {
+                try {
+                    stage('Checkout') {
+                        // check out the source code from the same repository that the Jenkinsfile resides in.
+                        checkout scm
+                    }
 
-                stage('Fetch Maven Settings') {
+                    stage('Fetch Maven Settings') {
                         mavenBuildSettings(gitCredentialsId, mavenSettingRepo, mavenSettingRepoBranch)
-                }
+                    }
 
-                stage('Build') {
-                    sh 'mvn clean package'
-                }
+                    stage('Build') {
+                        sh 'mvn clean package'
+                    }
 
-                stage('Test') {
-                    sh 'mvn test'
-                }
+                    stage('Test') {
+                        sh 'mvn test'
+                    }
 
-                stage('Deploy') {
-                    echo 'Deploying application...'
-                // Add deployment steps here
+                    stage('Deploy') {
+                        echo 'Deploying application...'
+                    // Add deployment steps here
+                    }
+                } catch (Exception ex) {
+                    currentBuild.result = 'FAILURE'
+                    echo 'Pipeline failed.'
+                    throw ex
+                } finally {
+                    if (currentBuild.result == 'SUCCESS') {
+                        echo 'Pipeline succeeded!'
+                    }
+                    echo 'Cleaning up...'
                 }
-        } catch (Exception ex) {
-                currentBuild.result = 'FAILURE'
-                echo 'Pipeline failed.'
-                throw ex
-        } finally {
-                if (currentBuild.result == 'SUCCESS') {
-                    echo 'Pipeline succeeded!'
-                }
-                echo 'Cleaning up...'
             }
         }
     }
